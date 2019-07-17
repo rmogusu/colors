30404f6199d993876cce5e4a5d06d26e98465a79
mrsfreddy <rmogusu123@gmal.com>
2019-07-17 10:16:06 -0400

    Add content to my project

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// $(document).ready(function() {
+//         $("button#green").click(function() {
+//           $("body").addClass("green-background");
+//         });
+//
+//         $("button#yellow").click(function() {
+//           $("body").addClass("yellow-background");
+//         });
+//
+//         $("button#red").click(function() {
+//           $("body").addClass("red-background");
+//         });
+//       });
+
+      $(document).ready(function() {
+              $("button#green").click(function() {
+                $("body").removeClass();
+                $("body").addClass("green-background");
+              });
+
+              $("button#yellow").click(function() {
+                $("body").removeClass();
+                $("body").addClass("yellow-background");
+              });
+
+              $("button#red").click(function() {
+                $("body").removeClass();
+                $("body").addClass("red-background");
+              });
+            });
